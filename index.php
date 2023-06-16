2f2e315f8b514724013d1a4df231fd7e8e50513e
unknown <ethan.nicolle.pro@gmail.com>
2023-06-16 03:17:41 +0200

    Adding a data file to store all the data.

--- index.php
+++ index.php
@@ -1,35 +1,6 @@
 <?php 
 
-    $url = "https://randomuser.me/api/";
-    $params = array('results' => 1);
-
-    $queryString = http_build_query($params);
-    $apiUrl = $url . "?" . $queryString;
-
-    $response = file_get_contents($apiUrl);
-    $data = json_decode($response, true);
-
-    $users = $data['results'];
-
-    $photo = $users[0]['picture']['large'];
-
-    // Name informations
-    $title = $users[0]['name']['title'];
-    $firstname = $users[0]['name']['first'];
-    $lastname = $users[0]['name']['last'];
-
-    // Birth informations
-    $age = $users[0]['dob']['age'];
-    $dateofbirth = $users[0]['dob']['date'];
-    $dob = strtok($dateofbirth, "T");
-
-    // Location informations
-    $streetnumber = $users[0]['location']['street']['number'];
-    $streetname = $users[0]['location']['street']['name'];
-    $postcode = $users[0]['location']['postcode'];
-    $city = $users[0]['location']['city'];
-    $state = $users[0]['location']['state'];
-    $country = $users[0]['location']['country'];
+require_once 'data.php';
 
 ?>
 
@@ -59,7 +30,6 @@
                 <p><?= $postcode . " " . $city?></p>
                 <p><?= $country ?></p>
             </div>
-
         </div>
 
     </div>
